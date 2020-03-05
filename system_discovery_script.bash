187f7ec55506bd812513d95fde978f2f205237cc
Teddy Knab <31280061+zeekus@users.noreply.github.com>
2020-03-05 09:37:47 -0500

    Create system_discovery_script.bash
    added a system discovery script

---
+++ system_discovery_script.bash
@@ -0,0 +1,66 @@
+#!/bin/bash
+#filename: system_discovery_script.bash
+
+function my_header {
+    echo
+    echo
+    echo "*************************************"
+    echo $TITLE
+    echo "*************************************"
+    echo
+}
+
+RPM_PATH="/usr/bin/rpm"
+IP="/usr/sbin/ip"
+HOSTS="/etc/hosts"
+
+TITLE="PACKAGES INSTALLED"
+
+if [ -e $RPM_PATH ]; then
+  P_COUNT=`rpm -qa| wc -l`
+  my_header
+  $RPM_PATH -qai
+  TITLE="$P_COUNT $TITLE"
+  my_header
+fi
+
+
+if [ -e $IP ]; then
+  TITLE="IP ROUTE "
+  my_header
+  $IP route
+  TITLE="IP ADDRESS"
+  my_header
+  $IP addr
+fi
+
+if [ -e $HOSTS ]; then
+   TITLE="HOSTS FILE"
+   my_header
+   cat $HOSTS
+fi
+
+if [ -e /etc/redhat-release ];then
+   TITLE="RED HAT RELEASE"
+   my_header
+   cat /etc/redhat-release
+fi
+
+TITLE="DISK SPACE"
+my_header
+df -h
+
+TITLE="DISK INODES"
+my_header
+df -i
+
+TITLE="UPTIME"
+my_header
+uptime 
+
+TITLE="PROCESSES"
+my_header
+ps aux
+
+TITLE="MESSAGES"
+dmesg
