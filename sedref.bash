6c6d4bc7671c2da528309845450f39626b2f0d5f
Teddy Knab <31280061+zeekus@users.noreply.github.com>
2020-07-09 08:24:39 -0400

    Update sedref.bash

--- sedref.bash
+++ sedref.bash
@@ -1,2 +1,10 @@
+#!/bin/bash
+#filename: sedref.bash
+#description: sed replace reference
+
 #find all files with pp and replace clean_metadata with centos6_clean_metadata
 sed -i  's/clean_metadata/centos6_clean_metadata/g' $(find . -type f -name '*.pp')
+
+#Converted all my names using sed: 
+sed -i 's/ec2_fw/centos6_legacy_ec2_fw/g' *.pp
+
