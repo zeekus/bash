3531f63302fc2bddc309a1e1fb3117afdfe62e7c
Teddy Knab <31280061+zeekus@users.noreply.github.com>
2020-07-14 11:19:13 -0400

    Update check_puppet_ssl_certificates.sh

--- check_puppet_ssl_certificates.sh
+++ check_puppet_ssl_certificates.sh
@@ -2,12 +2,15 @@
 #filename: check_puppet_ssl_certs.sh
 #description: check expirations on puppet certificates.
 
-cat ($find /etc/puppetlabs/puppet/ssl -iname 'ca.pem') | openssl x509 -enddate
 
 list_of_files=$(find /etc/puppetlabs/puppet/ssl -iname '*.pem')
+#echo $list_of_files
 for file in $list_of_files
   do
-    expiration=$(cat $file| openssl x509 -enddate)
-    echo $file $expiration
+    expiration=$(cat $file | openssl x509 -enddate -noout 2>> /dev/null)
+
+    if [ ! -z "$expiration" ]; then
+      echo $file $expiration
+    fi
   done
 
