f684a3d3ac63323f1873c51bf72ad0282c0e7430
Teddy Knab <31280061+zeekus@users.noreply.github.com>
2020-07-23 16:32:31 -0400

    simple_add_a_user.bash

---
+++ simple_add_a_user.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+#filename: simple_add_a_user.bash
+#description: add a user from a bash script
+
+user="admin"
+group=wheel
+home="/home/$user"
+shell="/bin/bash"
+pass="someexamplepassword"
+
+create_user() {
+        useradd -d $home -G $group -m -s $shell $user
+        if [ $? == 0 ]
+        then
+           echo "successfuly added user"
+        fi
+}
+
+reset_pass(){
+       echo "$user:$pass" | chpasswd
+       if [ $? == 0 ]
+       then
+          echo "successfuly updated password"
+       fi
+}
+
+
+uid=$(id $user)
+if [ $? == 0 ]
+then
+   echo user exists reseting pass
+   reset_pass
+else
+   echo user does not exist creating..
+   create_user
+   reset_pass
+fi
