379dd5bfab4d4e21b3c6eae5f7d6b33a4cce4332
Teddy Knab <31280061+zeekus@users.noreply.github.com>
2020-08-07 10:42:26 -0400

    Create one_instance_backup.sh
    Example run:
    [myuser@myserver backups]$ sh one_instance_backup.sh
    my instance name is CENTOS8_LINUX_AMI with instance of i-0852c2b4e72154218
    volume id: vol-0d5bb081dc9435cfa
    ......snap name is CENTOS8_LINUX_AMI_i-0852c2b4e72154218_vol-0d5bb081dc9435cfa_08_07_2020
    ......running .... aws ec2 create-snapshot --volume-id vol-0d5bb081dc9435cfa --query 'SnapshotId' --description 'CENTOS8_LINUX_AMI_i-0852c2b4e72154218_vol-0d5bb081dc9435cfa_08_07_2020'
    ......... snapshot created with a snapshotid of "snap-067f09525e6c964d2"

---
+++ one_instance_backup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#filename: one_instance_backup.sh
+#author: Theodore Knab
+#date: 06/25/2020
+#License Type: GNU GENERAL PUBLIC LICENSE, Version 3
+#description make snapshots of the Linux hosts in aws
+
+target='CENTOS8_LINUX_AMI' #instance name
+
+mydate=$(date +%m_%d_%Y) #get date
+
+#get instances
+#linux_instances=$(aws ec2 describe-instances --filters Name=tag:"Operating System",Values=*inux* --query 'Reservations[].Instances[].InstanceId' --output text)
+#target_instances=$(aws ec2 describe-instances --filters Name=tag:"Name",Values=*test_ami* --query 'Reservations[].Instances[].InstanceId' --output text)
+target_instances=$(aws ec2 describe-instances --filters Name=tag:"Name",Values=$target --query 'Reservations[].Instances[].InstanceId' --output text)
+
+#loop through the instance names
+for myinstance in $target_instances
+do
+  #get instance name
+  instance_name=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$myinstance" "Name=key,Values=Name" --query 'Tags[].Value' --output text)
+  echo my instance name is $instance_name with instance of $myinstance
+
+  #get the volume id for the instance
+  volume_ids=$( aws ec2 describe-volumes --filters "Name=attachment.instance-id,Values=$myinstance" "Name=attachment.status, Values=attached" --query 'Volumes[].Attachments[].VolumeId' --output text)
+
+
+  #make a snapshot of each volume attached to the instnace
+  for volume in $volume_ids
+    do
+      echo "volume id: $volume"
+      snap_name="${instance_name}_${myinstance}_${volume}_${mydate}"
+      echo "......snap name is $snap_name"
+      echo "......running .... aws ec2 create-snapshot --volume-id $volume --query 'SnapshotId' --description '$snap_name' "
+
+      snapshotid=$(aws ec2 create-snapshot --volume-id $volume --query 'SnapshotId' --description "$snap_name" )
+      echo "......... snapshot created with a snapshotid of $snapshotid"
+    done
+
+done
