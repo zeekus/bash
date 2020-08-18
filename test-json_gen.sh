4de6a90a73e372b52088f52e19de71122d06b2b2
Theodore Knab <tknab@chesapeakebay.net>
2020-08-18 17:01:48 -0400

    edit

---
+++ test-json_gen.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#filename: test-json.sh
+#description: automatically registers a domain and ptr record in route53 for AWS using json files.
+#author: Theodore Knab
+
+HOSTED_ZONE_ID="XXXXXXXX1"
+REVERSE_ZONE_ID="XXXXXXX2"
+DOMAIN="myexample.com"
+AWS_REGION=us-east-1
+hostname="myhostname"
+PRIV_IPV4=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+IPBRIF=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | awk -F "." '{print $4"." $3}')
+ARPA="$IPBRIF.128.10.in-addr.arpa."
+
+
+revfile="rev_file.json"
+dnsfile="dns_file.json"
+
+#json for dns record
+cat > $dnsfile << EOF
+{
+ "Comment": "R53DNS.sh created A for $hostname.$DOMAIN",
+ "Changes": [{
+   "Action": "UPSERT",
+   "ResourceRecordSet": { 
+     "Name": "$hostname.$DOMAIN." ,
+     "Type": "A",
+     "TTL" : 300,
+     "ResourceRecords": [{
+        "Value": "$PRIV_IPV4"
+     }]
+  }
+ }]
+}
+
+EOF
+    
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file://$dnsfile
+
+#json for PTR record
+cat > $revfile << EOF
+
+{
+ "Comment": "R53DNS.sh created PTR for $hostname.$DOMAIN",
+ "Changes": [{
+   "Action": "UPSERT",
+   "ResourceRecordSet": { 
+     "Name": "$ARPA" ,
+     "Type": "PTR",
+     "TTL" : 300,
+     "ResourceRecords": [{
+        "Value": "$hostname.$DOMAIN."
+     }]
+  }
+ }]
+}
+
+EOF
+
+aws route53 change-resource-record-sets --hosted-zone-id $REVERSE_ZONE_ID --change-batch file://$revfile
