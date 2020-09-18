8db83c7f04e801f8a9c9d7b94795020d92e2436b
Theodore Knab <tknab@chesapeakebay.net>
2020-09-18 09:53:18 -0400

    edit

--- generic_reference_sed.bash
+++ generic_reference_sed.bash
@@ -8,3 +8,11 @@ sed -i  's/clean_metadata/centos6_clean_metadata/g' $(find . -type f -name '*.pp
 #Converted all my names using sed: 
 sed -i 's/ec2_fw/centos6_legacy_ec2_fw/g' *.pp
 
+
+#remove quotes from string
+quoted_str=\"something\"
+echo 1 $quoted_str
+unq_string=$(echo $quoted_str| sed 's/"//g')
+echo 2 $unq_sring
+
+
