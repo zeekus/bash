d230aa709b2d43644d01634cc51373d90bbecf00
Teddy Knab <31280061+zeekus@users.noreply.github.com>
2020-10-07 09:39:14 -0400

    Update aws_cli_ec2_read_tags_local.sh

--- aws_cli_ec2_read_tags_local.sh
+++ aws_cli_ec2_read_tags_local.sh
@@ -1,8 +1,6 @@
- #!/bin/bash
- #filename: aws_cli_ec2_read_tags_local.sh
- #description: read local host's aws tags
- instance-id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
- aws ec2 describe-tags --filters "Name=resource-id,Values=${instance-id}
- 
- availabiltiy-zone=$(curl -s http://169.254.169.254/latest/meta-data/availabiltiy-zone)
- 
+#!/bin/bash
+#filename: aws_cli_ec2_read_tags_local.sh
+#description: read local host's aws tags
+instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+echo $instance_id
+aws ec2 describe-tags --filters "Name=resource-id,Values=${instance_id}"
