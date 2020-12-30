f2988e5c910020f230d8e77e75cce9db456b2554
Theodore Knab <tknab@chesapeakebay.net>
2020-12-30 10:04:28 -0500

    added function

--- bash_kerberos_health_check.sh
+++ bash_kerberos_health_check.sh
@@ -2,18 +2,31 @@
 #!filename: kerberos_health_checker.sh.
 #description: make sure kerbose is still functional on a Linux host.
 
-user="mydomain-user"
+user="mydomainuser"
 DOMAIN="MYDOMAIN"
+
+join_domain() {
+
+   if [[ -e "/usr/sbin/realm" ]]; then #use realm
+     realm join -U $user $DOMAIN
+   elif [[ -e "/usr/sbin/adcli" ]]; then  #use adcli
+     adcli join --domain=$DOMAIN --login-user=$user
+     #adcli join  --domain-controller=dce.example.net --domain-ou='ou=Computers,ou=Linux,dc=example,dc=net' --login-user='$user'
+   else
+     echo "unknown paramater"
+   fi
+}
 
 #check for a keytab file
 if [[ -e "/etc/krb5.keytab" ]]; then
    echo "keytab file exists: It looks like we have a domain registration."
 else
    echo "the /etc/krb5.keytab file is missing. It looks like Kerbose is not setup right. Re-join the domain with 'adcli' or 'realmd'"
-   #rejoin the domain ?
+   #rejoin the domain
+   join_domain
 fi
 
-#check for an active ticket 
+#check for an active ticket
 klist_check=$(klist -l | grep -i persistent| wc -l)
 
 if [[ klist_check -eq 1 && $? == 0 ]] ; then
