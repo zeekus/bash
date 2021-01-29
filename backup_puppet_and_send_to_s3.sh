ff447d038a054c37201a48ff2fa9968ad5a4a95d
Theodore Knab <tknab@chesapeakebay.net>
2021-01-29 12:07:35 -0500

    edit

--- backup_puppet_and_send_to_s3.sh
+++ backup_puppet_and_send_to_s3.sh
@@ -5,28 +5,31 @@
 #description: backs up puppet and sends it to an s3 bucket
 #requirements: aws-cli, aws-crendentials or role, an s3 bucket
 
-debug=1
+debug=0
 
 #get my hostname
 myhost=$(hostname| awk -F"." {'print $1'})
+echo $myhost
 
-s3_bucket="puppet-backups/$myhost/"
+s3_bucket="backups"
 
 #backup puppet
 /opt/puppetlabs/bin/puppet-backup  create
 
-#this creates a backup log 
+#this creates a backup log
 #get the last backup filename from the backup log
 backup_file_name=$(cat /var/log/puppetlabs/pe-backup-tools/backup.log | tail -1| sed -e s/.*filename:\ //g | sed -e s/\)$//g)
 
 if [[ -e $backup_file_name ]];then
   if [ $debug == 1 ] ; then
-    mycmd="aws s3 mv $backup_file_name s3://$my_s3_bucket/puppet/$myhost/ --dryrun"
+    mycmd="aws s3 cp $backup_file_name s3://$s3_bucket/puppet/$myhost/ --dryrun"
     logger "running $mycmd"
+    echo "running $mycmd"
     $mycmd
   else
-    mycmd="aws s3 mv $backup_file_name s3://$my_s3_bucket/puppet/$myhost/ "
+    mycmd="aws s3 cp $backup_file_name s3://$s3_bucket/puppet/$myhost/ "
     logger "running $mycmd"
+    echo "running $mycmd"
     $mycmd
   fi
 else
