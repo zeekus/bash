dbeafc11ed031b05c4356ba05dd9baa71b2b3537
Theodore Knab <ipmasqman@gmail.com>
2021-11-16 12:05:26 -0500

    edit

--- rsync_user_dirs.sh
+++ rsync_user_dirs.sh
@@ -7,12 +7,13 @@ list=$(grep -i :1002 /etc/passwd) #common user group is 1002
 echo "#start of commands"
 
 destination_ip="10.1.1.1"
+ssh_key_name="mykey.pem"
 
 
 for line in $list
 do
    mydir=$(echo $line | awk -F: {'print $6'} )
    #echo $mydir
-   sync_command="sudo rsync -avxP --rsync-path=\"sudo rsync\" -e \"ssh -i /home/centos/.ssh/cloudfish.pem\" $mydir centos@$destination_ip:$mydir"
+   sync_command="sudo rsync -avxP --rsync-path=\"sudo rsync\" -e \"ssh -i /home/centos/.ssh/$ssh_key_name\" $mydir centos@$destination_ip:$mydir"
    echo $sync_command
 done
