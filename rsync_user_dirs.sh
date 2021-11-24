ec1535ebff58ed61cd6041c0e94048064f63a617
Theodore Knab <ipmasqman@gmail.com>
2021-11-24 10:25:38 -0500

    edit

--- rsync_user_dirs.sh
+++ rsync_user_dirs.sh
@@ -7,13 +7,28 @@ list=$(grep -i :1002 /etc/passwd) #common user group is 1002
 echo "#start of commands"
 
 destination_ip="10.1.1.1"
-ssh_key_name="mykey.pem"
+ssh_key_name="/home/centos/.ssh/mykey.pem"
+run_file="/var/tmp/rsync_run.sh"
 
+if [[ -e $run_file ]];then
+   echo "error $run_file already exists. You may want to remove it."
+   exit
+else
+   #initialize file
+   echo > $run_file
+   chmod +x $run_file
+fi
 
 for line in $list
 do
    mydir=$(echo $line | awk -F: {'print $6'} )
-   #echo $mydir
-   sync_command="sudo rsync -avxP --rsync-path=\"sudo rsync\" -e \"ssh -i /home/centos/.ssh/$ssh_key_name\" $mydir centos@$destination_ip:$mydir"
-   echo $sync_command
+   sync_command="sudo rsync -avxP --rsync-path=\"sudo rsync\" -e \"ssh -i $ssh_key_name\" $mydir centos@$destination_ip:$mydir"
+   echo "appending: $sync_command"
+   echo $sync_command >> $run_file
 done
+
+echo "run sync"
+bash $run_file
+
+
+
