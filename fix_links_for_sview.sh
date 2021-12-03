501663e1fe728d44f8536eb9b8d2712ae1419b83
Theodore Knab <ipmasqman@gmail.com>
2021-12-03 10:33:15 -0500

    edit

---
+++ fix_links_for_sview.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#filename: fix_links_for_sview.sh
+#description: allows user to install sview on a aws-parallelcluster as a no dep
+#author: Theodore Knab
+#date: 12/3/21
+#Copyright (C) 2007 Free Software Foundation, Inc
+#licence ref GPL https://www.gnu.org/licenses/gpl-3.0.txt
+
+#download rpm
+yum install slurm-gui --downloadonly
+
+#install rpm with no deps
+rpm -ivh /var/cache/yum/x86_64/7/epel/packages/slurm-gui-20.11.8-2.el7.x86_64.rpm --nodeps
+
+#put slurm libaries where sview expects them. 
+slurm_libs=$(ls /opt/slurm/lib/slurm)
+mkdir -p /usr/lib64/slurm/
+for file in $slurm_libs
+do
+  echo $file
+  ln -s /opt/slurm/lib/slurm/$file /usr/lib64/slurm/$file
+done
+
+#put the configs in a place sview can find them
+ln -s /opt/slurm/etc/slurm.conf /etc/slurm/slurm.conf
+
+#remove if empty dir
+if [ -e /etc/slurm ]; then 
+  rmdir /etc/slurm
+fi 
+
+#create symbolic link so sview can find the configs
+ln -s /opt/slurm/etc /etc/slurm
