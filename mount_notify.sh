05b6056b4b00574d864d910ac6bdbfed06ba31f4
Theodore Knab <ipmasqman@gmail.com>
2022-03-29 14:33:18 -0400

    updated script

--- mount_notify.sh
+++ mount_notify.sh
@@ -3,15 +3,23 @@
 #description: check mountpoint cron
 
 mountpoints="/media/share /media/imagery"
-myemail="myuser@myexample.com"
+myuser="someuser@someip.net"
+mycopy="myuser@someip.net"
 
 for mount in $mountpoints
 do
 
   if mountpoint -q "$mount"; then
-    echo "$mount is a mountpoint"
+    echo "All Ok.. $mount is a mountpoint"
   else
-    echo "$mount is not a mountpoint"
-    echo "missing imagery mountpoints" | mail -s "missing mount point $mount on lgis3d" $myemail
+cat << EOL |  mutt -s "missing imagery mountpoints" $myuser -c $mycopy -F /root/muttrc
+It appears that we are missing the imagery Mountpoints.
+Please mount them ASAP.
+
+Directions:
+       mount -t cifs -o username=myuser,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm  //server1/WrkGrp  /media/share
+       mount -t cifs -o username=myuser,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm //server2/ImageryServer /media/imagery
+EOL
+exit
   fi
 done
