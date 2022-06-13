2bb07f6c9ff7fb8b72d99ffdc77668f383ae2fa4
Theodore Knab <ipmasqman@gmail.com>
2022-06-13 11:58:27 -0400

    updated comments

--- bash_one_liners.sh
+++ bash_one_liners.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#filename: get_top_100_largest_dirs.sh
 #get a list of the top 100 largest directories in home with a low threshold of 1GB
 #sort data in reverse order with the highest number first
 du -ah -t 1G /home | sort -r -n  | head -100 
