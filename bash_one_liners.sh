1142134ce3d97d30a37dab8242086d915fd7c621
Theodore Knab <ipmasqman@gmail.com>
2022-08-12 11:07:43 -0400

    updated

--- bash_one_liners.sh
+++ bash_one_liners.sh
@@ -4,10 +4,17 @@
 #sort data in reverse order with the highest number first
 du -ah -t 1G /home | sort -r -n  | head -100 
 
-
 #find disk space on each user in the home dir
 find /home -maxdepth 1 -exec du -hs {} ';'
 
+#get a list of all the rpms,the install date, and version 
+rpm -qa --qf '(%{INSTALLTIME:date}): %{NAME}-%{VERSION}\n' > /var/tmp/installed_rpm_list.txt
+
+#get all the installed rpms
+rpm -qa
+
+#get the yum repos in use
+yum repolist
 
 
 
